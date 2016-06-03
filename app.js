07fd6c4cf98fd777a8c3460e81e11d42352b9da0
Isael Feliciano <rogelioisael@hotmail.com>
2016-06-03 14:10:30 -0400

    Add functionality
    Add button, add funtion to populate list of process searched

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //win.showDevTools();
 const exec = require('child_process').exec;
 
-function searchProcess() {
-	location = "#second-page";
+function quickill() {
+//	location = "#second-page";
 	var processToFind = $(".txt-search").val();
 	exec('tasklist | find /I "' + processToFind + '"', (err, sto, ste) => {
 		if (err) {
@@ -20,4 +20,30 @@ function searchProcess() {
 			console.log(`Process killed, ${sto}`);
 		});
 	});
+}
+
+function searchProcess() {
+	location = "#second-page";
+	$('.process-list').empty();
+	var processToFind = $(".txt-search").val();
+	exec('tasklist | find /I "' + processToFind + '"', (err, sto, ste) => {
+		if (err) {
+			console.log(`exec error: ${error}`);
+			return;
+		}
+		sto = sto.split("\n");
+		sto.pop();
+		console.log(`sto: ${sto.length}`);
+		var processToKill = sto[0].match(/[^\\]*\.(\w+)/g);
+		sto.forEach(logArrayElements);
+	});
+}
+
+function up() {
+	location = "#first-page";
+}
+
+function logArrayElements(el, index, array) {
+	var fileName = el.match(/[^\\]*\.(\w+)/g);
+	$('.process-list').append(`<input type="checkbox" value="${fileName}">${el}<br>`);
 }
